10441b87fb3181c68cb105ef3367fbea81d75ac2
Erika Signal <erika.signal@gmail.com>
2024-01-22 14:36:05 +0100

    Add test and code for step 1

--- src/main/java/uppgift2/Calculator.java
+++ src/main/java/uppgift2/Calculator.java
@@ -1,2 +1,17 @@
-package uppgift2;public class Calculator {
+package uppgift2;
+
+public class Calculator {
+
+    public int add(String numbers) {
+        if (numbers.isEmpty()){
+            return 0;
+        } else {
+            String[] numArray = numbers.split(",");
+            if (numArray.length == 1){
+                return Integer.parseInt(numArray[0]);
+            } else {
+                return Integer.parseInt(numArray[0]) + Integer.parseInt(numArray[1]);
+            }
+        }
+    }
 }
--- src/test/java/uppgift2/CalculatorTest.java
+++ src/test/java/uppgift2/CalculatorTest.java
@@ -1,4 +1,15 @@
+package uppgift2;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class CalculatorTest {
-  
+    @Test
+    public void testAddEmptyStringReturnsZero(){
+        Calculator calculator = new Calculator();
+        int result = calculator.add("");
+
+        assertEquals(0, result);
+    }
 }
