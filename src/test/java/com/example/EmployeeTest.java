583da2868dd17541d24da06f5e6c93a1be273611
Erika Signal <erika.signal@gmail.com>
2024-01-25 16:49:56 +0100

    Add test for set id and salary
    Also moved EmployeeToString test to EmployeesTest to see complete coverage

--- src/test/java/com/example/EmployeeTest.java
+++ src/test/java/com/example/EmployeeTest.java
@@ -5,9 +5,9 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class EmployeeTest {
-    @Test
-    public void testEmployeeToString(){
-        Employee employee = new Employee("1", 30000);
-        assertEquals("Employee [id=1, salary=30000.0]", employee.toString());
-    }
+//    @Test
+//    public void testEmployeeToString(){
+//        Employee employee = new Employee("1", 30000);
+//        assertEquals("Employee [id=1, salary=30000.0]", employee.toString());
+//    }
 }
--- src/test/java/com/example/EmployeesTest.java
+++ src/test/java/com/example/EmployeesTest.java
@@ -26,4 +26,29 @@ class EmployeesTest {
         assertTrue(employee1.isPaid());
         assertTrue(employee2.isPaid());
     }
+
+    //Moved test here to see coverage clearer
+    @Test
+    public void testEmployeeToString(){
+        Employee employee = new Employee("1", 30000);
+        assertEquals("Employee [id=1, salary=30000.0]", employee.toString());
+    }
+
+    @Test
+    public void testSetID(){
+        Employee employee = new Employee("1", 17000);
+
+        employee.setId("1");
+
+        assertEquals("1", employee.getId());
+    }
+
+    @Test
+    public void testSetSalary(){
+        Employee employee = new Employee("1", 23500);
+
+        employee.setSalary(25000);
+
+        assertEquals(25000, employee.getSalary());
+    }
 }
